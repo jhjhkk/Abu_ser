19291035bd4b568a81744d92c7b6743357c5856d
jhjhkk <102557302+jhjhkk@users.noreply.github.com>
2022-03-31 19:32:59 +0000

    Update adan.js

--- plugins/adan.js
+++ plugins/adan.js
@@ -12,11 +12,11 @@ const Config = require('../config');
 const adzan = "Finds prayer time."
 const usage = ".prayer <city>"
 
-const butuh = "```give a city name!```"
-const renek = "```Not found the city!```"
+const butuh = "```اسم مدينة أرجوك!```"
+const renek = "```المدينة عير موجودة!```"
 
 
-        Asena.addCommand({pattern: 'adan ?(.*)', desc: adzan, usage: usage, fromMe: false}, async (message, match) => {
+        Asena.addCommand({pattern: 'adan|أذان ?(.*)', desc: adzan, usage: usage, fromMe: false}, async (message, match) => {
 
 	    if (match[1] === '') return await message.reply(butuh);
 	    const url = `https://api.pray.zone/v2/times/today.json?city=${match[1]}`;
@@ -26,12 +26,12 @@ const renek = "```Not found the city!```"
 		    if (response.statusCode === 200) return await message.client.sendMessage(message.jid,  '*Jadwal Sholat*:\n' +
                     '📅 ```' + json.results.datetime[0].date.gregorian + ' | ' + json.results.location.timezone + '```\n' +
                     '🌏 ```' + json.results.location.city + ' | ' + json.results.location.country + '```\n\n' +
-		    '```Imsak   :``` *' + json.results.datetime[0].times.Imsak + '*\n' + 
-		    '```Subuh   :``` *' + json.results.datetime[0].times.Fajr + '*\n' +
-		    '```Dzuhur  :``` *' + json.results.datetime[0].times.Dhuhr + '*\n' + 
-		    '```Asar    :``` *' + json.results.datetime[0].times.Asr + '*\n' + 
-		    '```Maghrib :``` *' + json.results.datetime[0].times.Maghrib + '*\n' +
-        '```Isha    :``` *' + json.results.datetime[0].times.Isha + '*\n', MessageType.text);
+		    '```الفجر   :``` *' + json.results.datetime[0].times.Imsak + '*\n' + 
+		    '```الصبح   :``` *' + json.results.datetime[0].times.Fajr + '*\n' +
+		    '```الضهر  :``` *' + json.results.datetime[0].times.Dhuhr + '*\n' + 
+		    '```العصر    :``` *' + json.results.datetime[0].times.Asr + '*\n' + 
+		    '```المغرب :``` *' + json.results.datetime[0].times.Maghrib + '*\n' +
+        '```العشاء    :``` *' + json.results.datetime[0].times.Isha + '*\n', MessageType.text);
 	    } catch {
 		    return await message.client.sendMessage(message.jid, renek, MessageType.text);
 	    }
